8515d9b1c8707f8a9117b9dd295f8aef55f338d6
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-01-22 21:14:51 +0200

    Update counter workflow.

--- backend/demo-worker/internal/counter/model.go
+++ backend/demo-worker/internal/counter/model.go
@@ -1,29 +1,51 @@
 package counter
 
+import (
+	"fmt"
+	"time"
+)
+
 const TaskQueueName = "demo_counter"
 
 type Counter struct {
-	Name     string `json:"name"`
-	MaxValue int    `json:"max_value"`
-	Value    int    `json:"value"`
+	Name        string        `json:"name"`
+	MaxValue    int           `json:"max_value"`
+	Value       int           `json:"value"`
+	IsExpired   bool          `json:"is_expired"`
+	TTL         string        `json:"ttl"`
+	TTLDuration time.Duration `json:"-"`
+}
+
+type CounterResult struct {
+	Value     int  `json:"value"`
+	IsExpired bool `json:"is_expired"`
 }
 
 type IncrementCounterSignal struct {
 	Value int `json:"value"`
 }
 
-func NewCounter(name string, maxValue int) Counter {
-	return Counter{
-		Name:     name,
-		MaxValue: maxValue,
-		Value:    0,
+func (c *Counter) Validate() error {
+	var err error
+	c.TTLDuration, err = time.ParseDuration(c.TTL)
+	if err != nil {
+		return fmt.Errorf("parse ttl: %w", err)
 	}
+	return nil
+}
+
+func (c *Counter) IsReady() bool {
+	return c.IsExpired || c.MaxValue < c.Value
 }
 
 func (c *Counter) Increment(value int) {
 	c.Value += value
 }
 
-func (c *Counter) IsReady() bool {
-	return c.MaxValue < c.Value
+func (c *Counter) Reset() {
+	c.Value = 0
+}
+
+func (c *Counter) SetExpired() {
+	c.IsExpired = true
 }
--- backend/demo-worker/internal/counter/workflow.go
+++ backend/demo-worker/internal/counter/workflow.go
@@ -5,36 +5,53 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-func CounterWorkflow(ctx workflow.Context, state Counter) error {
+func CounterWorkflow(ctx workflow.Context, state Counter) (*CounterResult, error) {
 	logger := workflow.GetLogger(ctx)
 
 	logger.Info("start counter", "counter", state)
+
+	if err := state.Validate(); err != nil {
+		return nil, err
+	}
 
 	err := workflow.SetQueryHandler(ctx, "get_counter", func() (Counter, error) {
 		return state, nil
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	incrementCounterChannel := workflow.GetSignalChannel(ctx, "increment_counter")
+	resetCounterChannel := workflow.GetSignalChannel(ctx, "reset_counter")
+
+	selector := workflow.NewSelector(ctx)
+
+	selector.AddReceive(incrementCounterChannel, func(c workflow.ReceiveChannel, _ bool) {
+		var signal any
+		c.Receive(ctx, &signal)
+
+		var message IncrementCounterSignal
+		err := mapstructure.Decode(signal, &message)
+		if err != nil {
+			logger.Error("invalid signal type %v", err)
+			return
+		}
+
+		state.Increment(message.Value)
+	})
+
+	selector.AddReceive(resetCounterChannel, func(c workflow.ReceiveChannel, _ bool) {
+		var signal any
+		c.Receive(ctx, &signal)
+
+		state.Reset()
+	})
+
+	selector.AddFuture(workflow.NewTimer(ctx, state.TTLDuration), func(f workflow.Future) {
+		state.SetExpired()
+	})
 
 	for {
-		selector := workflow.NewSelector(ctx)
-		selector.AddReceive(incrementCounterChannel, func(c workflow.ReceiveChannel, _ bool) {
-			var signal any
-			c.Receive(ctx, &signal)
-
-			var message IncrementCounterSignal
-			err := mapstructure.Decode(signal, &message)
-			if err != nil {
-				logger.Error("invalid signal type %v", err)
-				return
-			}
-
-			state.Increment(message.Value)
-		})
-
 		selector.Select(ctx)
 
 		if state.IsReady() {
@@ -44,5 +61,8 @@ func CounterWorkflow(ctx workflow.Context, state Counter) error {
 
 	logger.Info("stop counter", "counter", state)
 
-	return nil
+	return &CounterResult{
+		Value:     state.Value,
+		IsExpired: state.IsExpired,
+	}, nil
 }
