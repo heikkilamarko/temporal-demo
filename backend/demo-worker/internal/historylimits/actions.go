0b6eb11f971f8c889ad708cc29786f166b3493e9
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-01-25 22:37:56 +0200

    Update deps.

--- backend/demo-worker/internal/historylimits/actions.go
+++ backend/demo-worker/internal/historylimits/actions.go
@@ -2,6 +2,6 @@ package historylimits
 
 import "context"
 
-func HistoryLimitsActivity(ctx context.Context, payloadSizeBytes int) ([]byte, error) {
-	return make([]byte, payloadSizeBytes), nil
+func HistoryLimitsActivity(ctx context.Context, resultSizeBytes int) ([]byte, error) {
+	return make([]byte, resultSizeBytes), nil
 }
